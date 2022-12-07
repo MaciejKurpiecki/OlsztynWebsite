765680bc11a7d10aee7929f6f2120cd8d5a992a5
Maciej Kurpiecki <recon4173@wp.pl>
2022-12-07 19:39:53 +0100

    let => const

--- js/script.js
+++ js/script.js
@@ -4,16 +4,16 @@
     }
 
     const onChangeBackgroundClick = () => {
-        let dayNight = document.querySelector(".js-dayNight");
-        let photo = document.querySelector(".js-landscape");
-        let buttonPicture = document.querySelector(".js-button");
+        const dayNight = document.querySelector(".js-dayNight");
+        const photo = document.querySelector(".js-landscape");
+        const buttonPicture = document.querySelector(".js-button");
 
         dayNight.classList.toggle("colorNight");
         photo.classList.toggle("header__pictureBannerNight");
         buttonPicture.classList.toggle("header__dayNightButton--moon");
     }
     const init = () => {
-        let changeBackgroundButton = document.querySelector(".js-button");
+        const changeBackgroundButton = document.querySelector(".js-button");
         changeBackgroundButton.addEventListener("click", onChangeBackgroundClick);
         welcome();
     };
