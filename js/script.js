3d7cc24c842f09a48fef0897e8f3ebf460069e1c
Maciej Kurpiecki <recon4173@wp.pl>
2022-12-07 19:15:31 +0100

    const init test pret

--- js/script.js
+++ js/script.js
@@ -1,21 +1,23 @@
 {
     const welcome = () => {
         console.log("Hello there!")
-    }
-    welcome();
-}
-{
+    };
+
 
     const onChangeBackgroundClick = () => {
         dayNight.classList.toggle("colorNight");
         photo.classList.toggle("header__pictureBannerNight");
         changeBackgroundButton.classList.toggle("header__buttonSun");
         changeBackgroundButton.classList.toggle("header__buttonMoon");
-    }
+    };
 
-    let changeBackgroundButton = document.querySelector(".js-button");
-    let dayNight = document.querySelector(".js-dayNight");
-    let photo = document.querySelector(".js-landscape");
+    const init = () => {
+        let changeBackgroundButton = document.querySelector(".js-button");
+        let dayNight = document.querySelector(".js-dayNight");
+        let photo = document.querySelector(".js-landscape");
 
-    changeBackgroundButton.addEventListener("click", onChangeBackgroundClick);
-}
+        changeBackgroundButton.addEventListener("click", onChangeBackgroundClick);
+        welcome();
+    };
+    init();
+};
