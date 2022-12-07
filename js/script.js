54f25a1935f8c3081a9fcf3e53cfd897caf232b6
Maciej Kurpiecki <recon4173@wp.pl>
2022-12-07 19:36:07 +0100

    daynight button script fixed

--- js/script.js
+++ js/script.js
@@ -1,23 +1,21 @@
 {
     const welcome = () => {
         console.log("Hello there!")
-    };
-
+    }
 
     const onChangeBackgroundClick = () => {
-        dayNight.classList.toggle("colorNight");
-        photo.classList.toggle("header__pictureBannerNight");
-        changeBackgroundButton.classList.toggle("header__buttonSun");
-        changeBackgroundButton.classList.toggle("header__buttonMoon");
-    };
-
-    const init = () => {
-        let changeBackgroundButton = document.querySelector(".js-button");
         let dayNight = document.querySelector(".js-dayNight");
         let photo = document.querySelector(".js-landscape");
+        let buttonPicture = document.querySelector(".js-button");
 
+        dayNight.classList.toggle("colorNight");
+        photo.classList.toggle("header__pictureBannerNight");
+        buttonPicture.classList.toggle("header__dayNightButton--moon");
+    }
+    const init = () => {
+        let changeBackgroundButton = document.querySelector(".js-button");
         changeBackgroundButton.addEventListener("click", onChangeBackgroundClick);
-        welcome();
     };
+    welcome();
     init();
-};
+}
